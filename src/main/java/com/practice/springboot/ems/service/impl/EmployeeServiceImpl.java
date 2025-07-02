a3a02df298b293abd1366beca3ee94968fcccffa
Harsh Vyas <harsh.vyas467@gmail.com>
2025-07-02 14:00:25 +0530

    build getEmployee rest api with exception handling

--- src/main/java/com/practice/springboot/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/practice/springboot/ems/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.practice.springboot.ems.service.impl;
 
 import com.practice.springboot.ems.dto.EmployeeDto;
 import com.practice.springboot.ems.entity.Employee;
+import com.practice.springboot.ems.exception.ResourceNotFoundException;
 import com.practice.springboot.ems.mapper.EmployeeMapper;
 import com.practice.springboot.ems.repository.EmployeeRepository;
 import com.practice.springboot.ems.service.EmployeeService;
@@ -21,5 +22,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee does not exist with given id : "+ employeeId));
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
