643a52792c907bf8ab8d716a75adbad69675036d
Harsh Vyas <harsh.vyas467@gmail.com>
2025-08-04 08:01:31 +0530

    Configured custom Spring Security for a REST API by disabling CSRF, enabling stateless HTTP Basic authentication, and defining two in-memory users (postman, admin) with encrypted passwords using BCrypt

---
+++ src/main/java/com/practice/springboot/ems/configuration/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.practice.springboot.ems.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//                    .formLogin(Customizer.withDefaults())
+                    .httpBasic(Customizer.withDefaults())
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            return http.build();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+            UserDetails user1 = User.builder()
+                    .username("postman")
+                    .password(passwordEncoder.encode("postman"))
+                    .roles("POSTMAN")
+                    .build();
+
+            UserDetails user2 = User.builder()
+                    .username("admin")
+                    .password(passwordEncoder.encode("admin123"))
+                    .roles("ADMIN")
+                    .build();
+
+            return new InMemoryUserDetailsManager(user1, user2);
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+}
